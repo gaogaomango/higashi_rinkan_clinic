34541c4eb9d4591835a7f75fac6fe6e9ec81db77
Ogai Masataka <gaogaomango@gmail.com>
2016-12-30 04:39:52 +0900

    パスの指定の仕方を修正

--- js/contents.js
+++ js/contents.js
@@ -1,55 +1,45 @@
          function loadContents(){
+            // 共通部分
+            if($("#title")[0] != null){    
+                $("#title").load("/header.html");
+            }
+            if($("#footer")[0] != null){
+                $("#footer").load("/footer.html");                    
+            }
+            if($("#clinickMap")[0] != null){
+                $("#clinickMap").load("/contents_map.html")
+            }
+            if($("#openTime")[0] != null){
+                $("#openTime").load("/open_time.html");                
+            }
+
             var $dir = location.href.split("/");
             var $dir1 = $dir[3];
             // 往診
             if($dir1 === "house_calls"){
-                if($("#title")[0] != null){    
-                    $("#title").load("../header.html");
-                }
-                if($("#footer")[0] != null){
-                    $("#footer").load("../footer.html");                    
-                }
                 if($("#contentsHeader")[0] != null){
-                    $("#contentsHeader").load("contents_header_house_calls.html");
+                    $("#contentsHeader").load("/house_calls/contents_header_house_calls.html");
                 }
                 if($("#contentsApplicationFlow")[0] != null){
-                    $("#contentsApplicationFlow").load("contents_application_flow.html");
+                    $("#contentsApplicationFlow").load("/house_calls/contents_application_flow.html");
                 }
                 if($("#contentsApplication")[0] != null){
-                    $("#contentsApplication").load("contents_application.html");
-                }
-                if($("#clinickMap")[0] != null){
-                    $("#clinickMap").load("../contents_map.html")
-                }
-                if($("#openTime")[0] != null){
-                    $("#openTime").load("../open_time.html");                
+                    $("#contentsApplication").load("/house_calls/contents_application.html");
                 }
                 if($("#clinickAddress")[0] != null){
-                    $("#clinickAddress").load("contents_address.html");                
+                    $("#clinickAddress").load("/house_calls/contents_address.html");                
                 }
             // 外来
             } else {
-                if($("#title")[0] != null){                    
-                    $("#title").load("header.html");
-                }
-                if($("#footer")[0] != null){
-                    $("#footer").load("footer.html");                    
-                }
                 if($("#contentsHeader")[0] != null){
-                    $("#contentsHeader").load("contents_header.html");
+                    $("#contentsHeader").load("/contents_header.html");
                 }
                 if($("#clinickFeature")[0] != null){
-                    $("#clinickFeature").load("clinick_feature.html");
+                    $("#clinickFeature").load("/clinick_feature.html");
                 }
                 if($("#staffIntroduction")[0] != null){
-                    $("#staffIntroduction").load("staff_introduction.html");
+                    $("#staffIntroduction").load("/staff_introduction.html");
                 }
-                if($("#clinickMap")[0] != null){
-                    $("#clinickMap").load("contents_map.html")
-                }
-                if($("#openTime")[0] != null){
-                    $("#openTime").load("open_time.html");                
-                }                
             }
          }
          //共通パーツ読み込み
