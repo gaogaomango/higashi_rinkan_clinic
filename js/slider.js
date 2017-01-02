508d8657cdee93ff08a96b42c7363cadd23a87a1
Ogai Masataka <gaogaomango@gmail.com>
2017-01-02 22:37:21 +0900

    html調整。

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,28 @@
-$(function(){
-	 $('#slider').sliderPro({
-			width: '100%',
-			slideDistance:0,
-			arrows: true,
-			autoHeight:true,
-			buttons: false,
-			fadeFullScreen:true,
-			waitForLayers: true,
-			thumbnailWidth: 200,
-			thumbnailHeight: 100,
-			thumbnailPointer: true,
-			autoplay: false,
-			autoScaleLayers: false,
-			breakpoints: {
-				500: {
-					thumbnailWidth: 120,
-					thumbnailHeight: 50
-				}
-			}
-		});
+// $(function(){
+// 	 $('#slider').sliderPro({
+// 			width: '100%',
+// 			slideDistance:0,
+// 			arrows: true,
+// 			autoHeight:true,
+// 			buttons: false,
+// 			fadeFullScreen:true,
+// 			waitForLayers: true,
+// 			thumbnailWidth: 200,
+// 			thumbnailHeight: 100,
+// 			thumbnailPointer: true,
+// 			autoplay: false,
+// 			autoScaleLayers: false,
+// 			breakpoints: {
+// 				500: {
+// 					thumbnailWidth: 120,
+// 					thumbnailHeight: 50
+// 				}
+// 			}
+// 		});
+// });
+
+$("#featureBtn").click(function(){
+	$("html,body").animate({scrollTop:$('#clinickFeature').offset().top});
+	return false;
 });
+
